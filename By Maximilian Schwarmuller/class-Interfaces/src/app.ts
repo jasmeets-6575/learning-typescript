dbacf88c700704d62227ac92b430f59eef9dc160
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-26 21:42:48 +0530

    TS classes and interfaces

--- By Maximilian Schwarmuller/class-Interfaces/src/app.ts
+++ By Maximilian Schwarmuller/class-Interfaces/src/app.ts
@@ -1,10 +1,29 @@
 class Department {
-  name:string;
-  
-  constructor(n: string) {
-    this.name = n;
+    public name: string;
+    private employees: string[] = [];
+    constructor(n: string) {
+        this.name = n;
+    }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting")
-console.log(accounting);
+const accounting = new Department("Accounting");
+accounting.addEmployee("John");
+accounting.addEmployee("Cena");
+
+
+// accounting.employees[2] = "Anna"
+accounting.describe();
+accounting.printEmployeeInformation();
