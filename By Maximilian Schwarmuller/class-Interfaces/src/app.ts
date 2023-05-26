48a5ae70bec3b2df092706151df649aea5b9150d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-27 00:50:47 +0530

    TS classes and interfaces class

--- By Maximilian Schwarmuller/class-Interfaces/src/app.ts
+++ By Maximilian Schwarmuller/class-Interfaces/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   //   private id: string;
   //   public name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -33,11 +33,18 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
   }
-  addReport(text:string) {
-    this.reports.push(text)
+
+  addEmployee(name: string) {
+    if (name === "John") {
+      return;
+    }
+    this.employees.push(name);
   }
-  
-  printReport(){
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReport() {
     console.log(this.reports);
   }
 }
@@ -53,6 +60,10 @@ it.describe();
 it.printEmployeeInformation();
 
 const accounting = new AccountingDepartment("A1", []);
-accounting.addReport("Something went wrong")
-accounting.addReport("Some Error Occurs")
-accounting.printReport()
+
+accounting.addEmployee("John");
+accounting.addEmployee("Sam");
+accounting.printEmployeeInformation();
+accounting.addReport("Something went wrong");
+accounting.addReport("Some Error Occurs");
+accounting.printReport();
