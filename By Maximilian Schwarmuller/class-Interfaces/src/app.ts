faf97d62d386e95be05ae9aeb4be652df17a4774
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-26 22:32:52 +0530

    TS classes and interfaces class

--- By Maximilian Schwarmuller/class-Interfaces/src/app.ts
+++ By Maximilian Schwarmuller/class-Interfaces/src/app.ts
@@ -1,12 +1,15 @@
 class Department {
-    public name: string;
-    private employees: string[] = [];
-    constructor(n: string) {
-        this.name = n;
-    }
+  //   private id: string;
+  //   public name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -14,16 +17,42 @@ class Department {
   }
 
   printEmployeeInformation() {
-    console.log(this.employees.length);
     console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
-accounting.addEmployee("John");
-accounting.addEmployee("Cena");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
 
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+  addReport(text:string) {
+    this.reports.push(text)
+  }
+  
+  printReport(){
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new Department("d1", "Accounting");
+const it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("John");
+it.addEmployee("Cena");
 
 // accounting.employees[2] = "Anna"
-accounting.describe();
-accounting.printEmployeeInformation();
+
+it.describe();
+it.printEmployeeInformation();
+
+const accounting = new AccountingDepartment("A1", []);
+accounting.addReport("Something went wrong")
+accounting.addReport("Some Error Occurs")
+accounting.printReport()
