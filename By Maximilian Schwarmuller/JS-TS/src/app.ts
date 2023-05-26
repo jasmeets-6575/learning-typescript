04a32b3b83c7bb087e0686edaf3192dea433e792
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-26 15:55:05 +0530

    TS js arrow functions

--- By Maximilian Schwarmuller/JS-TS/src/app.ts
+++ By Maximilian Schwarmuller/JS-TS/src/app.ts
@@ -18,14 +18,37 @@ age = 29;
 
 // console.log(result);
 
-const add = (a: number, b: number = 3) => a + b;
+// const add = (a: number, b: number = 3) => a + b;
 
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
 
-const button = document.querySelector("button");
-if (button) {
-  button.addEventListener("click", (event) => console.log(event));
-}
+// const button = document.querySelector("button");
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
+
+const hobbies = ["sports", "cooking"];
+const activeHobbies = ["hiking"];
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const person = {
+  name: "john",
+  age: 30,
+};
+
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curr, acc) => {
+    return curr + acc;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 3, 2, 3.7);
+console.log(addedNumbers);
