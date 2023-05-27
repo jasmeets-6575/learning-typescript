e2d69e54b3c726115baff04a04e7992a4ce56c5a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-28 02:50:00 +0530

    TS generic function

--- By Maximilian Schwarmuller/generics/src/app.ts
+++ By Maximilian Schwarmuller/generics/src/app.ts
@@ -17,3 +17,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 // console.log(merge({ name: "max" }, { age: 30 }));
 const mergedObj = merge({ name: "max" }, { age: 30 });
 console.log(mergedObj.age);
+
+// *******************\
+
+interface Lengthy {
+  length: number;
+}
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = "Got No value";
+  if (element.length === 1) {
+    description = "got 1 element";
+  } else if (element.length > 1) {
+    description = "got " + element.length + " element";
+  }
+  return [element, description];
+}
+
+console.log(countAndDescribe("Hi there"));
