7cf31802503f6755ec15bcf0fa49f196f4dce795
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-29 00:12:25 +0530

    TS using js types

---
+++ By Maximilian Schwarmuller/library-and-typescript/src/product.model.ts
@@ -0,0 +1,12 @@
+export class Product {
+    title: string;
+    price: number;
+
+    constructor(t:string,p:number) {
+        this.title = t;
+        this.price = p;
+    }
+    getInfo() {
+        return [this.title, `$${this.price}`]
+    }
+}
