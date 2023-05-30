894fed57aaa08e55ef52758514da49f08cfb8996
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-30 23:49:53 +0530

    TS using react

--- By Maximilian Schwarmuller/react-Ts/my-app/src/App.tsx
+++ By Maximilian Schwarmuller/react-Ts/my-app/src/App.tsx
@@ -6,7 +6,10 @@ import { Todo } from "./todo.model";
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    setTodos([...todos, { id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
   return (
     <div>
