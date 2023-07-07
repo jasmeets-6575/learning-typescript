5ec90619d757f0535d03bf582f707c5b36cb8bd5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-07 16:07:13 +0530

    chapter-6 classes getter and setter

--- by-dave-gray/chap-6-classes/src/main.ts
+++ by-dave-gray/chap-6-classes/src/main.ts
@@ -38,3 +38,79 @@ const Sara = new WebDev("Mac", "Sara", "Lofi", 25);
 console.log(Sara.getLang());
 // console.log(Sara.age)
 // console.log(Sara.lang)
+
+/////////////////////////////////////
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play(action: string) {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+
+const Page = new Guitarist("Jimmy", "guitar");
+console.log(Page.play("strums"));
+
+//////////////////////////////////////
+
+class Peeps {
+  static count: number = 0;
+  static getCount(): number {
+    return Peeps.count;
+  }
+  public id: number;
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+
+console.log(Amy.id);
+console.log(Steve.id);
+console.log(John.id);
+console.log(Peeps.count);
+
+//////////////////////////////////
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("Param is not an array of strings");
+  }
+}
+
+const MyBands = new Bands();
+MyBands.data = ["Neil Young", "Led Zep"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "ZZ Top"];
+console.log(MyBands.data);
+// MyBands.data = ["Van Halen", 5150];
+// must be string data
