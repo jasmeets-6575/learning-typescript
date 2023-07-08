03712b2e80d5b87335d64ac9c153cc438eb9a684
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-08 18:23:30 +0530

    keyof typeof

--- by-dave-gray/chap-7-keyof-assertions/src/main.ts
+++ by-dave-gray/chap-7-keyof-assertions/src/main.ts
@@ -57,3 +57,23 @@ const logStudentKey = (student: Student, key: keyof Student): void => {
 };
 
 logStudentKey(student, "name");
+
+/////////////////////////////////
+
+// interface Incomes {
+//     [key: string]: number
+// }
+
+type Streams = "salary" | "bonus" | "sidehustle";
+
+type Incomes = Record<Streams, number>;
+
+const monthlyIncomes: Incomes = {
+  salary: 500,
+  bonus: 100,
+  sidehustle: 250,
+};
+
+for (const revenue in monthlyIncomes) {
+  console.log(monthlyIncomes[revenue as keyof Incomes]);
+}
