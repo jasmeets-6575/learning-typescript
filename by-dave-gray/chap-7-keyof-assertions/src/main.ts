439fc7cdae5aa78df9c933b4599ccc0b58f2d3e0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-08 18:10:51 +0530

    index signatures

--- by-dave-gray/chap-7-keyof-assertions/src/main.ts
+++ by-dave-gray/chap-7-keyof-assertions/src/main.ts
@@ -30,3 +30,30 @@ console.log(todaysNet(todaysTransactions));
 //todaysTransactions.Pizza = 40
 
 console.log(todaysTransactions["Dave"]); // undefined
+
+/////////////////////////////////////
+
+interface Student {
+  // [key: string]: string | number | number[] | undefined;
+  name: string;
+  GPA: number;
+  classes?: number[];
+}
+const student: Student = {
+  name: "jiohn",
+  GPA: 3.5,
+  classes: [100, 200],
+};
+
+for (const key in student) {
+  console.log(`${key}: ${student[key as keyof Student]}`);
+}
+
+Object.keys(student).map((key) => {
+  console.log(student[key as keyof typeof student]);
+});
+const logStudentKey = (student: Student, key: keyof Student): void => {
+  console.log(`Student ${key}: ${student[key]}`);
+};
+
+logStudentKey(student, "name");
