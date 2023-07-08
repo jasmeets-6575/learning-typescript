bc82961a49d7f7c10b6d2da523a9886752b3c290
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-08 17:52:56 +0530

    index signatures

--- by-dave-gray/chap-6-classes/src/main.ts
+++ by-dave-gray/chap-6-classes/src/main.ts
@@ -112,5 +112,4 @@ MyBands.data = ["Neil Young", "Led Zep"];
 console.log(MyBands.data);
 MyBands.data = [...MyBands.data, "ZZ Top"];
 console.log(MyBands.data);
-// MyBands.data = ["Van Halen", 5150];
-// must be string data
+// MyBands.data = ["Van Halen", 5150]; // must be string data
