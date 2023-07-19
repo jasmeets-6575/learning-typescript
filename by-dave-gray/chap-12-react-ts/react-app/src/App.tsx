9c1bb497473aa36d2b5984efa914d0452194e04e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-19 23:18:43 +0530

    react ts counter modified

--- by-dave-gray/chap-12-react-ts/react-app/src/App.tsx
+++ by-dave-gray/chap-12-react-ts/react-app/src/App.tsx
@@ -1,13 +1,17 @@
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
 import Section from "./components/Section";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
   return (
     <>
       <Heading title="home page" />
       <Section>This is my section</Section>
-      <Counter />
+      <Counter count={count} setCount={setCount}>
+        Total count is : {count}
+      </Counter>
     </>
   );
 }
--- by-dave-gray/chap-12-react-ts/react-app/src/components/Counter.tsx
+++ by-dave-gray/chap-12-react-ts/react-app/src/components/Counter.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import React, { ReactNode } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState<number>(0);
+type CounterProps = {
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+  children: ReactNode;
+  count: number;
+};
+
+const Counter = ({ count, children, setCount }: CounterProps) => {
   return (
     <>
-      <h1>Count is {count}</h1>
+      <h1>{children}</h1>
       <button onClick={() => setCount(count + 1)}>add </button>
       <button onClick={() => setCount(count - 1)}>subtract </button>
     </>
