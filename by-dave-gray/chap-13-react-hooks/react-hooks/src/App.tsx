072ecca9f031cb51f02688f9657290539bd07ff5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-19 23:52:00 +0530

    react hooks useState

--- by-dave-gray/chap-13-react-hooks/react-hooks/src/App.tsx
+++ by-dave-gray/chap-13-react-hooks/react-hooks/src/App.tsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
+
+interface User {
+  id: number;
+  userName: string;
+}
+
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const [user, setUser] = useState<User[] | null>(null);
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <h1>hello</h1>
