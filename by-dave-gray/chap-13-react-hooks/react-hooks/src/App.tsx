6cf985ab5a9af045dc30764bb764c610c668d42a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-20 00:08:02 +0530

    react hooks useCallback useMemo useRef

--- by-dave-gray/chap-13-react-hooks/react-hooks/src/App.tsx
+++ by-dave-gray/chap-13-react-hooks/react-hooks/src/App.tsx
@@ -1,24 +1,59 @@
-import { useState, useEffect } from "react";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useRef,
+  MouseEvent,
+  KeyboardEvent,
+} from "react";
 
 interface User {
   id: number;
-  userName: string;
+  username: string;
 }
+
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNum: number = 37;
 
 function App() {
   const [count, setCount] = useState<number>(0);
-  const [user, setUser] = useState<User[] | null>(null);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
 
   useEffect(() => {
     console.log("mounting");
-    console.log("User:", user);
+    console.log("Users: ", users);
+
     return () => console.log("unmounting");
-  }, [user]);
+  }, [users]);
+
+  const addTwo = useCallback(
+    (
+      e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+    ): void => setCount((prev) => prev + 2),
+    []
+  );
+
+  const result = useMemo<number>(() => fib(myNum), [myNum]);
 
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
+    </div>
   );
 }
 
