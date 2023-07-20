129a231fc6a67a96322c3e246d7f4c0bd77e1fbe
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-21 02:23:03 +0530

    adding counter text hook

--- by-dave-gray/chap-15-react-useContext/src/context/CounterContext.tsx
+++ by-dave-gray/chap-15-react-useContext/src/context/CounterContext.tsx
@@ -5,6 +5,7 @@ import {
   useReducer,
   ReactElement,
   useCallback,
+  useContext,
 } from "react";
 
 type StateType = {
@@ -83,4 +84,31 @@ export const CounterProvider = ({
       {children}
     </CounterContext.Provider>
   );
+};
+
+type useCounterHookType = {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+};
+export const useCounter = (): useCounterHookType => {
+  const {
+    state: { count },
+    increment,
+    decrement,
+  } = useContext(CounterContext);
+  return { count, increment, decrement };
+};
+
+type UseCounterTextHookType = {
+  text: string;
+  handleTextInput: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const useCounterText = (): UseCounterTextHookType => {
+  const {
+    state: { text },
+    handleTextInput,
+  } = useContext(CounterContext);
+  return { text, handleTextInput };
 };
