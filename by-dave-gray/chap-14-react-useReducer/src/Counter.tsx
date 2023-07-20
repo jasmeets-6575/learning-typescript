312ac75c779c7bb3365e63edfefc279257d9a1b5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-20 20:22:20 +0530

    react hooks use reducer

--- by-dave-gray/chap-14-react-useReducer/src/Counter.tsx
+++ by-dave-gray/chap-14-react-useReducer/src/Counter.tsx
@@ -1,5 +1,21 @@
 import { ReactNode, useReducer, ChangeEvent } from "react";
 
+const reducer = (
+  state: typeof initState,
+  action: ReducerAction
+): typeof initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case REDUCER_ACTION_TYPE.NEW_INPUT:
+      return { ...state, text: action.payload ?? "" };
+    default:
+      throw new Error();
+  }
+};
+
 type ChildrenType = {
   children: (num: number) => ReactNode;
 };
