b893547fa634a18f5a6be2f0ca32a8a2cae4400c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-21 17:44:10 +0530

    adding cart provider reducer

--- by-dave-gray/chap-16-react-project/vite-project/src/context/Cartprovider.tsx
+++ by-dave-gray/chap-16-react-project/vite-project/src/context/Cartprovider.tsx
@@ -31,10 +31,45 @@ const reducer = (
 ): CartStateType => {
   switch (action.type) {
     case REDUCER_ACTION_TYPE.ADD: {
+      if (!action.payload) {
+        throw new Error("action.payload missing in ADD action");
+      }
+      const { sku, name, price } = action.payload;
+      const filteredCart: CartItemType[] = state.cart.filter(
+        (item) => item.sku !== sku
+      );
+      const itemExists: CartItemType | undefined = state.cart.find(
+        (item) => item.sku === sku
+      );
+      const qty: number = itemExists ? itemExists.qty + 1 : 1;
+      return { ...state, cart: [...filteredCart, { sku, name, price, qty }] };
     }
     case REDUCER_ACTION_TYPE.REMOVE: {
+      if (!action.payload) {
+        throw new Error("action.payload missing in REMOVE action");
+      }
+      const { sku } = action.payload;
+      const filteredCart: CartItemType[] = state.cart.filter(
+        (item) => item.sku !== sku
+      );
+      return { ...state, cart: [...filteredCart] };
     }
     case REDUCER_ACTION_TYPE.QUANTITY: {
+      if (!action.payload) {
+        throw new Error("action.payload missing in QUANTITY action");
+      }
+      const { sku, qty } = action.payload;
+      const itemExists: CartItemType | undefined = state.cart.find(
+        (item) => item.sku === sku
+      );
+      if (!itemExists) {
+        throw new Error("Item must exist in order to update quantity");
+      }
+      const updatedItem: CartItemType = { ...itemExists, qty };
+      const filteredCart: CartItemType[] = state.cart.filter(
+        (item) => item.sku !== sku
+      );
+      return { ...state, cart: [...filteredCart, updatedItem] };
     }
     case REDUCER_ACTION_TYPE.SUBMIT: {
       return { ...state, cart: [] };
